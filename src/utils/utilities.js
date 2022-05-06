0f3aebabc36c4330f73bed9972864e10ed50481a
rabiulalam1 <rabiul.alam@outlook.com>
2022-05-05 21:02:11 -0400

    1 - 5

---
+++ src/utils/utilities.js
@@ -0,0 +1,7 @@
+export const rgbToHex = (r, g, b) => {
+  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+};
+
+export const randomNumber = (max = 1, min = 0) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
